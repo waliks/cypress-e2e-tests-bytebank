c2d683c96a2552b2d9af008fe76453e6d5e62fe0
waliks <walikslucas2007@hotmail.com>
2023-09-09 01:30:35 -0300

    implement more account creation test cases

--- cypress/e2e/createAccount.cy.js
+++ cypress/e2e/createAccount.cy.js
@@ -1,3 +1,5 @@
+import { faker } from '@faker-js/faker';
+
 describe('Create an account', () => {
 
   context('Given that I access the account creation form', () => {
@@ -11,12 +13,13 @@ describe('Create an account', () => {
 
       it('Then an account is succesfully created', () => {
 
-        cy.getByDataTest('nome-input').clear().type('automated tester')
-        cy.getByDataTest('email-input').clear().type('automated@teste.co')
-        cy.getByDataTest('senha-input').clear().type('tester123')
+        cy.getByDataTest('nome-input').clear().type(faker.person.fullName())
+        cy.getByDataTest('email-input').clear().type(faker.internet.email())
+        cy.getByDataTest('senha-input').clear().type(faker.internet.password())
 
         cy.intercept('**/register').as('registerAccount')
         cy.getByDataTest('botao-enviar').click()
+        cy.checkText('span', 'Usuário cadastrado com sucesso!')
 
         cy.wait('@registerAccount').then((call) => {
           expect(call.response.statusCode).to.equal(201)
@@ -24,6 +27,46 @@ describe('Create an account', () => {
         
       })
     })
+
+    context('When I submit an empty name', () => {
+
+      it('Then a message informs that the name is a mandatory field', () => {
+
+        cy.getByDataTest('nome-input').clear()
+        cy.getByDataTest('email-input').clear().type(faker.internet.email())
+        cy.getByDataTest('senha-input').clear().type(faker.internet.password())
+
+        cy.getByDataTest('botao-enviar').click()
+        cy.checkText('[data-test="mensagem-erro"]', 'O campo de nome é obrigatório')         
+      })
+    })
+
+    context('When I submit an empty email', () => {
+
+      it('Then a message informs that the email is a mandatory field', () => {
+
+        cy.getByDataTest('nome-input').clear().type(faker.person.fullName())
+        cy.getByDataTest('email-input').clear()
+        cy.getByDataTest('senha-input').clear().type(faker.internet.password())
+
+        cy.getByDataTest('botao-enviar').click()
+        cy.checkText('[data-test="mensagem-erro"]', 'O campo de email é obrigatório')         
+      })
+    })
+
+    context('When I submit an empty password', () => {
+
+      it('Then a message informs that the password is a mandatory field', () => {
+
+        cy.getByDataTest('nome-input').clear().type(faker.person.fullName())
+        cy.getByDataTest('email-input').clear().type(faker.internet.email())
+        cy.getByDataTest('senha-input').clear()
+
+        cy.getByDataTest('botao-enviar').click()
+        cy.checkText('[data-test="mensagem-erro"]', 'O campo de senha é obrigatório')         
+      })
+    })
+
   })
   
 })
